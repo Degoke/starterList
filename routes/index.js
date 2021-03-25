6febda64f73e7ab5dba5cd2fdd1dad645b78dcc5
Dipo-Jaiye <durojaiyep@yahoo.co.uk>
2021-03-26 00:21:07 +0100

    Added routes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const projectRoutes = require("./projectRoutes");
+
+router.use("/api/project",projectRoutes);
+router.use("/api/users",userRoutes);
 
 module.exports = router;
