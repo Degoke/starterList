a2cc4a55e5ed445bd76377b1624e461d47a5dc3f
Dipo-Jaiye <durojaiyep@yahoo.co.uk>
2021-03-26 02:29:05 +0100

    new routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 
 router.get("/");
+router.post("/login");
+router.post("/logout");
 router.post("/create");
 router.put("/:id/update");
 router.delete("/:id/delete");
