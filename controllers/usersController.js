5b6aa2b77274d2a13f8b573d373f95df94fa4409
Dipo-Jaiye <durojaiyep@yahoo.co.uk>
2021-03-28 20:59:41 +0100

    At my wits end

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,7 +4,7 @@ const cloudinary = require('cloudinary').v2;
 
 function getUserParams(obj){
     if(obj.file){
-        cloudinary.uploader.upload(obj.file.profileImage.path)
+        cloudinary.uploader.upload(obj.file.profileImage.buffer)
         .then(image=>{
             let url = image.url;
             return {
