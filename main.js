d1edd1d8ee6d431c692912f85365785b46059d30
Dipo-Jaiye <durojaiyep@yahoo.co.uk>
2021-03-28 21:32:40 +0100

    extended urlencoding

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ db.once("open",()=>{
 app.set("port", process.env.PORT || 3000);
 app.use(cors());
 app.use(express.static("public"));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(passport.initialize());
 passport.use(User.createStrategy());
