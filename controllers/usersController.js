0c0b628a472dddfd3f74b16dd3d4b3fb465e3b81
Dipo-Jaiye <durojaiyep@yahoo.co.uk>
2021-03-28 21:19:39 +0100

    Buffer conversion

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,7 +4,7 @@ const cloudinary = require('cloudinary').v2;
 
 function getUserParams(obj){
     if(obj.file){
-        cloudinary.uploader.upload(obj.file.buffer)
+        cloudinary.uploader.upload(obj.file.buffer.toString('base64'))
         .then(image=>{
             let url = image.url;
             return {
